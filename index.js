500a544d97e7ab4a3944a500657108a9ec451ec9
adrile15 <adrile1501@gmail.com>
2023-02-24 12:17:16 +0100

    falta la media

---
+++ index.js
@@ -0,0 +1,23 @@
+function log(msg){
+    console.log(msg);
+}
+
+log("Mostramos a continuacion un Array de datos de un columna de mi propuesta");
+
+//var o = new Object();
+
+//var m= new Map();
+
+var immigrant = new Array();
+immigrant=[2.930,3.111,4.014,3.403,3.167,3.486,3.134,3.058,4.473,4.329];
+
+log(immigrant)
+
+
+immigrant.forEach(log);
+
+//immigrant.filter(log)
+
+
+
+
